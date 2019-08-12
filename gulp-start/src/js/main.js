b7ffc3ae7e69b59f070aba57f217a93093091a6b
Pasha Myndzul <51452458+PashaMyndzul@users.noreply.github.com>
2019-08-12 08:37:41 +0300

    Add files via upload

--- gulp-start/src/js/main.js
+++ gulp-start/src/js/main.js
@@ -3,3 +3,13 @@
     console.log("main");
 
 }
+async function sum() {
+    const a = await 9;
+    const b = await delayAndGetRandom(1000);
+    const c = await 5;
+    await delayAndGetRandom(1000);
+    
+    return a + b * c;
+  }
+  
+  fn().then(console.log)
